08998324e73c813012cdc69c53760c2d401dee20
Tarin Mahmood <mahmood.tarin+home@gmail.com>
2021-05-21 18:42:01 +0600

    Cleaned up code and added more tests

--- spring_entity_gen.py
+++ spring_entity_gen.py
@@ -3,6 +3,7 @@
 
 import sys
 import os.path
+import inflect
 
 MY_PATH = os.path.dirname(os.path.realpath(__file__))
 ENTITY_FILE = MY_PATH + '/entity_template.txt'
@@ -10,64 +11,112 @@ REPO_FILE = MY_PATH + '/repository_template.txt'
 CTRL_FILE = MY_PATH + '/controller_template.txt'
 AUDIT_MODEL_FILE = MY_PATH + '/audit_model_template.txt'
 
+def make_code_from_template(replacements, template):
+    template_content = None
+    try:
+        with open(template) as fp:
+            template_content = fp.read()
+        return template_content % replacements
+    except TypeError as e:
+        print(replacements)
+        print(template_content)
+        raise e
+    except Exception as e:
+        print(template)
+        print(replacements)
+        raise e
+
+
+def write_to_file(output, content):
+    with open(output, 'w') as fp:
+        fp.write(content)
+
 
 def write_from_template(replacements, template, output):
-    template_content = None
-    with open(template) as fp:
-        template_content = fp.read()
-    with open(output, 'w') as fp:
-        try:
-            fp.write(template_content % replacements)
-        except TypeError as e:
-            print(replacements)
-            print(template_content)
-            raise e
-        except Exception as e:
-            print(template)
-            print(replacements)
-            raise e
+    content = make_code_from_template(replacements, template)
+    write_to_file(output, content)
+
+
+def make_java_class_names(input_class_name, replacements):
+    """ 
+        @param: "Book Author"
+
+        @returns:
+            class name: BookAuthor
+            table name: book_authors
+            java variable name: bookAuthor
+            api endpoint: bookAuthors
+    """
+    func = lambda s: s[:1].lower() + s[1:] if s else ''
+    p = inflect.engine()
+    replacements['java_class_name'] = input_class_name.title().replace(' ', '')
+    replacements['camel_case'] = func(input_class_name).replace(' ', '')
+    replacements['camel_case_plural'] = p.plural(replacements['camel_case'])
+    replacements['snake_case_plural'] = p.plural(input_class_name.lower().replace(' ', '_'))
+
+
+class SpringEntityBuilder(object):
+
+    def __init__(self, java_class_name, package_name) -> None:
+        super().__init__()
+        self.java_class_name = java_class_name
+        self.template_vars = {'package_name': package_name}
+        make_java_class_names(java_class_name, self.template_vars)
+        self.java_src_package_path = 'src/main/java/%s' % package_name.replace('.', '/')
+        self.model_path = self.java_src_package_path + '/models'
+        self.repo_path = self.java_src_package_path + '/repositories'
+        self.controller_path = self.java_src_package_path + '/controllers'
+        self.model_path_with_name = '{}/{}.java'.format(
+            self.model_path, 
+            self.template_vars['java_class_name'])
+        self.repo_path_with_name = '{}/{}Repository.java'.format(
+            self.repo_path, 
+            self.template_vars['java_class_name'])
+        self.ctrl_path_w_name = '{}/{}Controller.java'.format(
+            self.controller_path,
+            self.template_vars['java_class_name'])
+
+
+def generate_from_entity_template(entityDetails):
+    write_from_template(entityDetails.template_vars, ENTITY_FILE, entityDetails.model_path_with_name)
+
+
+def generate_from_repo_template(entityDetails):
+    write_from_template(entityDetails.template_vars, REPO_FILE, entityDetails.repo_path_with_name)
+
+
+def generate_from_controller_template(entityDetails):
+    write_from_template(entityDetails.template_vars, CTRL_FILE, entityDetails.ctrl_path_w_name)
+
+
+def make_directories(entityDetails):
+    os.makedirs(entityDetails.java_src_package_path, exist_ok=True)
+    os.makedirs(entityDetails.model_path, exist_ok=True)
+    os.makedirs(entityDetails.repo_path, exist_ok=True)
 
 
 def main():
     """
     Entry point
     """
-    replacements = {}
-    class_name = sys.argv[1]
-    replacements['package_name'] = sys.argv[2]
+    entityDetails = SpringEntityBuilder(sys.argv[1], sys.argv[2])
     try:
         other_opts = sys.argv[3:]
     except IndexError:
         other_opts = []
-    java_src_package_path = 'src/main/java/%s' % sys.argv[2].replace('.', '/')
-    model_path = java_src_package_path + '/models'
-    repo_path = java_src_package_path + '/repositories'
-    os.makedirs(java_src_package_path, exist_ok=True)
-    os.makedirs(model_path, exist_ok=True)
-    os.makedirs(repo_path, exist_ok=True)
-    # make it camel case
-    jcn = class_name.title().replace(' ', '')
-    replacements['java_class_name'] = jcn
-    # make it snake case
-    ssc = class_name.lower().replace(' ', '_')
-    ssc_plurel = class_name.lower().replace(' ', '_') + 's'
-    replacements['sql_snake_case'] = ssc_plurel
-    replacements['sql_snake_case_single'] = ssc
+    make_directories(entityDetails)
     # write the model template
-    model_path_with_name = '{}/{}.java'.format(model_path, jcn)
-    write_from_template(replacements, ENTITY_FILE, model_path_with_name)
-    repo_path_with_name = '{}/{}Repository.java'.format(repo_path, jcn)
-    write_from_template(replacements, REPO_FILE, repo_path_with_name)
+    generate_from_entity_template(entityDetails)
+    generate_from_repo_template(entityDetails)
     if 'skp_cnt' not in other_opts:
-        controller_path = java_src_package_path + '/controllers'
-        os.makedirs(controller_path, exist_ok=True)
-        ctrl_path_w_name = '{}/{}Controller.java'.format(controller_path, jcn)
-        write_from_template(replacements, CTRL_FILE, ctrl_path_w_name)
-    if not os.path.exists(model_path + "AuditMode.java"):
-        write_from_template(replacements, AUDIT_MODEL_FILE, f'{model_path}/AuditModel.java')
+        os.makedirs(entityDetails.controller_path, exist_ok=True)
+        generate_from_controller_template(entityDetails)
+    auditPath = f'{entityDetails.model_path}/AuditModel.java'
+    if not os.path.exists(auditPath):
+        write_from_template(entityDetails.template_vars, AUDIT_MODEL_FILE, auditPath)
 
 
-msg = """Parameters missing: %s <class_name> <package_name> [skp_cnt]"""
+msg = """Parameters missing: %s <class_name in singular form i.e: "Book Author"> <package_name> [skp_cnt]"""
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print(msg % sys.argv[0].split('/').pop())
---
+++ tests.py
@@ -0,0 +1,134 @@
+import unittest
+import spring_entity_gen
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_class_name_process(self):
+        result = {}
+        spring_entity_gen.make_java_class_names("Book Author", result)
+        self.assertEqual(
+            result['java_class_name'],
+            "BookAuthor"
+        )
+        self.assertEqual(
+            result['camel_case_plural'],
+            "bookAuthors"
+        )
+        self.assertEqual(
+            result['snake_case_plural'],
+            "book_authors"
+        )
+        self.assertEqual(
+            result['camel_case'],
+            "bookAuthor"
+        )
+    
+    def test_generate_repository_from_template(self):
+        content = spring_entity_gen.make_code_from_template(
+            {
+                'java_class_name': 'BookAuthor',
+                'package_name': 'com.package'
+            }, 
+            spring_entity_gen.REPO_FILE, 
+        )
+        self.assertEqual(
+            content,
+            """package com.package.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+import com.package.models.BookAuthor;
+
+public interface BookAuthorRepository extends PagingAndSortingRepository<BookAuthor, Long> { 
+
+}
+"""
+        )
+
+
+    def test_generate_repository_from_entity(self):
+        content = spring_entity_gen.make_code_from_template(
+            {
+                'java_class_name': 'BookAuthor',
+                'snake_case_plural': 'book_authors',
+                'package_name': 'com.package'
+            }, 
+            spring_entity_gen.ENTITY_FILE, 
+        )
+        self.assertEqual(
+            content,
+            """package com.package.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import lombok.Data;
+import javax.persistence.*;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain=true)
+@Data
+@Entity
+@Table(name="book_authors")
+public class BookAuthor extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "book_authors_generator")
+    @SequenceGenerator(name="book_authors_generator", sequenceName = "book_authors_seq", allocationSize=1)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+}
+"""
+        )
+
+
+    def test_generate_controller_from_template(self):
+        self.maxDiff = None
+        content = spring_entity_gen.make_code_from_template(
+            {
+                'java_class_name': 'BookAuthor',
+                'camel_case_plural': 'bookAuthors',
+                'camel_case': 'bookAuthor',
+                'package_name': 'com.package'
+            }, 
+            spring_entity_gen.CTRL_FILE, 
+        )
+        self.assertEqual(
+            content,
+            """package com.package.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.package.models.BookAuthor;
+import com.package.repositories.BookAuthorRepository;
+
+@Controller    // This means that this class is a Controller
+@RequestMapping(path="/api/v1/bookAuthors")
+public class BookAuthorController {
+
+    private final BookAuthorRepository bookAuthorRepository;
+
+    @Autowired
+    public BookAuthorController(BookAuthorRepository bookAuthorRepository) {
+        this.bookAuthorRepository = bookAuthorRepository;
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<BookAuthor> getAllBookAuthor(Pageable page) {
+        return bookAuthorRepository.findAll(Pageable.unpaged());
+    }
+}
+"""
+        )
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
